675b5019ba6849768ddb00a9bbe2066181eb787a
josephzhang <junhua_zhang@sjtu.edu.cn>
2023-11-14 10:33:47 +0800

    make telescope reco py script

--- Examples/Scripts/Python/truth_tracking_telescope.py
+++ Examples/Scripts/Python/truth_tracking_telescope.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 
+import math
 import acts
 import acts.examples
 
@@ -11,19 +12,71 @@ u = acts.UnitConstants
 
 if "__main__" == __name__:
     detector, trackingGeometry, decorators = acts.examples.TelescopeDetector.create(
-        bounds=[200, 200],
-        positions=[30, 60, 90, 120, 150, 180, 210, 240, 270],
-        stereos=[0] * 9,
+        bounds=[268, 268],
+        positions=[5.33, 10.33, 15.33, 20.33, 55.33, 125.33, 235.33],
+        stereos=[-0.05, 0.05, -0.05, 0.05,-0.05, 0.05, -0.05],
     )
 
     srcdir = Path(__file__).resolve().parent.parent.parent.parent
 
-    field = acts.ConstantBField(acts.Vector3(0, 0, 2 * u.T))
+    field = acts.ConstantBField(acts.Vector3(0, -1.5 * u.T, 0))
+
+    rnd = acts.examples.RandomNumbers()
+
+    from acts.examples import (
+        RootSimHitReader,
+        RootParticleReader,
+    )
+
+    from acts.examples.reconstruction import (
+        addSeeding,
+        SeedingAlgorithm,
+        TruthSeedRanges,
+        addKalmanTracks,
+    )
+
+    s = acts.examples.Sequencer(
+        numThreads=-1, logLevel=acts.logging.INFO
+    )
+
+    s.addReader(
+        RootSimHitReader(
+            level=acts.logging.INFO,
+            filePath="Nov14Reco/dp_hits.root",
+            treeName="hits",
+            simHitCollection="simhits",
+        )
+    )
+    s.addReader(
+        RootParticleReader(
+            level=acts.logging.INFO,
+            filePath="Nov14Reco/dp_particles.root",
+            particleCollection="particles",
+            orderedEvents=False,
+        )
+    )
+
+    addSeeding(
+        trackingGeometry,
+        field,
+        seedingAlgorithm=SeedingAlgorithm.TruthSmeared,
+        rnd=rnd,
+        truthSeedRanges=TruthSeedRanges(
+            pt=(20 * u.MeV, None),
+            nHits=(4, None),
+            eta=(-15.0, 15.0),
+            rho=(0, 1000 * u.mm),
+            z=(0, 300 * u.mm),
+            phi=(-math.pi, math.pi),
+        ),
+    )
 
     runTruthTrackingKalman(
         trackingGeometry,
         field,
         digiConfigFile=srcdir
-        / "Examples/Algorithms/Digitization/share/default-smearing-config-telescope.json",
-        outputDir=Path.cwd(),
+                       / "Examples/Algorithms/Digitization/share/default-smearing-config-telescope.json",
+        outputDir=srcdir / "Nov14Reco",
+        s=s,
+        inputParticlePath=srcdir / "Nov14Reco/dp_particles.root"
     ).run()
